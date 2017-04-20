d7c1b95754f8876c54ac9aedd61799fe0cf9d61a
Ofir <Shalev@admins-MacBook-Pro-3.local>
2017-04-21 00:14:10 +0800

    refactor the questions text-files in a class structure, saving words not found in Glove as a sorted text file'

--- 02_TensorFlow_Way/05_Implementing_Back_Propagation/05_back_propagation.py
+++ 02_TensorFlow_Way/05_Implementing_Back_Propagation/05_back_propagation.py
@@ -72,8 +72,8 @@ sess = tf.Session()
 # Create data
 x_vals = np.concatenate((np.random.normal(-1, 1, 50), np.random.normal(3, 1, 50)))
 y_vals = np.concatenate((np.repeat(0., 50), np.repeat(1., 50)))
-x_data = tf.placeholder(shape=[1], dtype=tf.float32)
-y_target = tf.placeholder(shape=[1], dtype=tf.float32)
+x_data = tf.placeholder(shape=[1], dtype=tf.float32) # <tf.Tensor 'Placeholder:0' shape=(1,) dtype=float32>
+y_target = tf.placeholder(shape=[1], dtype=tf.float32) # <tf.Tensor 'Placeholder_1:0' shape=(1,) dtype=float32>
 
 # Create variable (one model parameter = A)
 A = tf.Variable(tf.random_normal(mean=10, shape=[1]))
@@ -81,18 +81,18 @@ A = tf.Variable(tf.random_normal(mean=10, shape=[1]))
 # Add operation to graph
 # Want to create the operstion sigmoid(x + A)
 # Note, the sigmoid() part is in the loss function
-my_output = tf.add(x_data, A)
+my_output = tf.add(x_data, A) # <tf.Tensor 'Add:0' shape=(1,) dtype=float32>
 
 # Now we have to add another dimension to each (batch size of 1)
-my_output_expanded = tf.expand_dims(my_output, 0)
-y_target_expanded = tf.expand_dims(y_target, 0)
+my_output_expanded = tf.expand_dims(my_output, 0) # <tf.Tensor 'ExpandDims:0' shape=(1, 1) dtype=float32>
+y_target_expanded = tf.expand_dims(y_target, 0) # <tf.Tensor 'ExpandDims_1:0' shape=(1, 1) dtype=float32>
 
 # Initialize variables
 init = tf.global_variables_initializer()
 sess.run(init)
 
 # Add classification loss (cross entropy)
-xentropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=my_output_expanded, labels=y_target_expanded)
+xentropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=my_output_expanded, labels=y_target_expanded) # <tf.Tensor 'logistic_loss:0' shape=(1, 1) dtype=float32>
 
 # Create Optimizer
 my_opt = tf.train.GradientDescentOptimizer(0.05)
